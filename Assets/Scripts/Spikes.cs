46dbd003ce030c98a8b2549c7ba92df18463a2bd
David Teinert <DavidTeinert344@t-online.de>
2023-06-14 12:09:37 +0200

    some changes

---
+++ Assets/Scripts/Spikes.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+    [SerializeField]
+    private int Damage = 100;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
