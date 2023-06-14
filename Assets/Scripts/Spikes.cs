0903e124d72e245fc8bb614e74621a8e62f8ef58
David Teinert <DavidTeinert344@t-online.de>
2023-06-14 12:29:20 +0200

    print something

--- Assets/Scripts/Spikes.cs
+++ Assets/Scripts/Spikes.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class Spikes : MonoBehaviour
+public class Spikes : Interactable
 {
     [SerializeField]
     private int Damage = 100;
@@ -10,6 +11,10 @@ public class Spikes : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    protected override void Interact(Collider other)
+    {
+        Debug.Log("1");
     }
 }
